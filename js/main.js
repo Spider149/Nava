103412be95690f29ea72a2e139c5f3ee8c03fc4f
spider149 <lehieu.149@gmail.com>
2022-10-03 22:05:09 +0700

    Animate numbers when scroll to

--- js/main.js
+++ js/main.js
@@ -1,23 +1,67 @@
 let menuBtn = document.querySelector(".menu__button");
 let nav = document.querySelector("nav");
 let body = document.querySelector("body");
+let sectionNumbers = document.querySelector(".numbers");
 
 menuBtn.addEventListener("click", () => {
-    console.log("!");
     nav.classList.toggle("open");
     menuBtn.classList.toggle("open");
 });
 
 document.addEventListener("click", (e) => {
-    console.log(e.target);
     if (
         nav.classList.contains("open") &&
         !e.target.classList.contains("nav__list") &&
         !e.target.parentNode.classList.contains("menu__button") &&
         !e.target.classList.contains("menu__button")
     ) {
-        console.log("2");
         nav.classList.remove("open");
         menuBtn.classList.remove("open");
     }
 });
+
+function debounce(fn, delay) {
+    return (args) => {
+        clearTimeout(fn.id);
+
+        fn.id = setTimeout(() => {
+            fn.call(this, args);
+        }, delay);
+    };
+}
+
+const startCounter = (element) => {
+    const duration = 1500;
+    const start = 0;
+    const end = parseInt(element.dataset.counter);
+
+    let startTime = Date.now();
+
+    const loop = () => {
+        let passedTime = Date.now() - startTime;
+        let currentNumber = parseInt(
+            start + (passedTime / duration) * (end - start)
+        );
+        if (currentNumber > end) currentNumber = end;
+        element.textContent = currentNumber.toLocaleString();
+        if (currentNumber < end) requestAnimationFrame(loop);
+    };
+
+    requestAnimationFrame(loop);
+};
+
+let handleScroll = () => {
+    let viewHeight =
+        window.innerHeight || document.documentElement.clientHeight;
+    let isVisible = sectionNumbers.getBoundingClientRect().top - viewHeight < 0;
+    if (isVisible) {
+        document.removeEventListener("scroll", handleScrollDebounced);
+        Array.from(sectionNumbers.children).forEach((item) => {
+            startCounter(item.querySelector("span"));
+        });
+    }
+};
+
+let handleScrollDebounced = debounce(handleScroll, 50);
+document.addEventListener("scroll", handleScrollDebounced);
+handleScroll();
